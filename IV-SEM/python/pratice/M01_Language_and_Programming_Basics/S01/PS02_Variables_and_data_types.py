5b4ae8df6dec64d07bf2d1305d102e00550b6e13
vemuri sai manoj <vemurisaimanoj19@gmail.com>
2026-01-09 11:53:50 +0530

    mes1

---
+++ IV-SEM/python/pratice/M01_Language_and_Programming_Basics/S01/PS02_Variables_and_data_types.py
@@ -0,0 +1,26 @@
+# Data type :
+# 1.Fundamental data types 
+#     1.int 
+#     2.float 
+#     3.bool 
+#     4.str
+#     5.complex 
+# 2.collection DT :
+#     1.List 
+#     2.Tuple 
+#     3.set
+#     4.Dictionary
+
+
+x = 10 
+y = 12e3 
+print(x,type(x))
+print(y , type(y))
+
+c1 = 4 + 5j 
+c2 = complex(2 , -2)
+print(c1.real)
+print(c2.imag)
+print(c1 + c2)
+print(c1*c2)
+print(c1/c2)
