ecd47cbd60230cee3056914157226752eb812324
saimanoj14 <193805051+saimanoj14@users.noreply.github.com>
2026-02-16 14:59:49 +0530

    assement 5

--- IV-SEM/python/assignments/M01_Language_&_Programming_Basics/AST05/task.py
+++ IV-SEM/python/assignments/M01_Language_&_Programming_Basics/AST05/task.py
@@ -1,8 +1,15 @@
 from typing import List
 
+def Collatz_Sequence(n: int) -> List[int]:
+    sequence = [n]  # start the sequence with n
+    while n != 1:
+        if n % 2 == 0:  # even
+            n = n // 2
+        else:  # odd
+            n = 3 * n + 1
+        sequence.append(n)
+    return sequence
 
-def Collatz_Sequence(n: int)-> List:
-   pass
 if __name__ == '__main__':
     n = int(input())
     print(Collatz_Sequence(n))
